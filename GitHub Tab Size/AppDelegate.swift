c6715a2fcf2e2eddc0dc2381a8ae0ff87da081eb
Julian Dunskus <julian.dunskus@gmail.com>
2020-01-11 18:11:27 +0100

    now quits after window closed

--- GitHub Tab Size/AppDelegate.swift
+++ GitHub Tab Size/AppDelegate.swift
@@ -2,11 +2,5 @@ import Cocoa
 
 @NSApplicationMain
 final class AppDelegate: NSObject, NSApplicationDelegate {
-	func applicationDidFinishLaunching(_ aNotification: Notification) {
-		// Insert code here to initialize your application
-	}
-	
-	func applicationWillTerminate(_ aNotification: Notification) {
-		// Insert code here to tear down your application
-	}
+	func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool { true }
 }
